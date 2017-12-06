c1cfe6d971e105dd319c41d399e07b0daf926ba4
Cantinaband <s.ruisinger@googlemail.com>
2017-12-06 02:27:20 +0100

    gitignore functions
    add gitignore
    change radius to correct 100000 meter rectangle => need right algorithm
    add my personal myql database and password

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
-const http = require('http');
-const mysql = require('mysql');
-const express = require('express');
-const url = require('url');
+const http = require("http");
+const mysql = require("mysql");
+const express = require("express");
+const url = require("url");
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'LostInSpace',
-    database: 'meteor'
+    host: "localhost",
+    user: "root",
+    password: "MeteorHistory",
+    database: "test"
 });
 
 const app = express();
-const radius = 4.52;
+const radius = 0.8;
 function getQueryInRadius(lat, lon) {
-    console.log("Lat: " + lat +": " + typeof lat);
-    let radiusQuery = 'SELECT * FROM meteorexample WHERE (lat BETWEEN ' + (lat-radius) + ' AND ' + (lat+radius) +') AND (lon BETWEEN ' + (lon-radius) + ' AND '+ (lon+radius) + ')';
+    console.log("Lat: " + lat + ": " + typeof lat);
+    let radiusQuery =
+        "SELECT * FROM test WHERE (lat BETWEEN " +
+        (lat - radius) +
+        " AND " +
+        (lat + radius) +
+        ") AND (lon BETWEEN " +
+        (lon - radius) +
+        " AND " +
+        (lon + radius) +
+        ")";
     return radiusQuery;
 }
 
 const tasks = {
     newMeteorites: {
-        name: "newMeteorites",
+        name: "newMeteorites"
     }
-}
+};
 
-connection.connect((err) => {
-    if(err) {
+connection.connect(err => {
+    if (err) {
         console.log("Error: " + err);
     }
 });
@@ -34,21 +43,23 @@ app.get("/", (request, response) => {
     let urlSended = url.parse(request.url, true);
     let query = urlSended.query;
     console.log(query);
-    response.setHeader('Content-Type', 'application/json');
+    response.setHeader("Content-Type", "application/json");
 
     if (query.task === tasks.newMeteorites.name) {
-        console.log("SERVER: Message - excecuting task: \"", query.task, "\"" );
+        console.log('SERVER: Message - excecuting task: "', query.task, '"');
 
-        connection.query(getQueryInRadius(parseFloat(query.lat), parseFloat(query.lon)), (err, rows, fields) => {
-            if(err) {
-                console.log("Error: ", err);
-            } else {
-                console.log("SERVER: Message - query solution: ", rows);
+        connection.query(
+            getQueryInRadius(parseFloat(query.lat), parseFloat(query.lon)),
+            (err, rows, fields) => {
+                if (err) {
+                    console.log("Error: ", err);
+                } else {
+                    console.log("SERVER: Message - query solution: ", rows);
 
-                response.send(JSON.stringify(rows));
+                    response.send(JSON.stringify(rows));
+                }
             }
-        });
+        );
     }
-
 });
 app.listen(8080);
