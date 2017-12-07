e568a61761f5e87c258c7997eb7e82812e02429c
Cantinaband <s.ruisinger@googlemail.com>
2017-12-08 00:07:14 +0100

    ad
    as

--- index.js
+++ index.js
@@ -60,7 +60,43 @@ function getContent(lat, lon) {
         ")";
 
     let test =
-        "select * from Country, events, Meteor where events.eventLat BETWEEN (-1.002 - 0.8) and (-1.002 + 0.8) and events.eventLon BETWEEN (37.15 - 0.8) and (37.15 + 0.8) and Country.lat BETWEEN (-1.002 - 0.8) and (-1.002 + 0.8) and Country.lon BETWEEN (37.15 - 0.8) and (37.15 + 0.8) and Meteor.lat BETWEEN (-1.002 - 0.8) and (-1.002 + 0.8) and Meteor.lon BETWEEN (37.15 - 0.8) and (37.15 + 0.8)";
+        "select * from Country, events, Meteor where events.eventLat BETWEEN (" +
+        lat -
+        lon +
+        ") and ( " -
+        1.002 +
+        0.8 +
+        ") and events.eventLon BETWEEN (" +
+        37.15 -
+        0.8 +
+        ") and (" +
+        37.15 +
+        0.8 +
+        ") and Country.lat BETWEEN (" +
+        -1.002 -
+        0.8 +
+        ") and (" +
+        -1.002 +
+        0.8 +
+        ") and Country.lon BETWEEN (" +
+        37.15 -
+        0.8 +
+        ") and (" +
+        37.15 +
+        0.8 +
+        ") and Meteor.lat BETWEEN (" +
+        -1.002 -
+        0.8 +
+        ") and (" +
+        -1.002 +
+        0.8 +
+        ") and Meteor.lon BETWEEN (" +
+        37.15 -
+        0.8 +
+        ") and (" +
+        37.15 +
+        0.8 +
+        ")";
     return test;
 }
 
