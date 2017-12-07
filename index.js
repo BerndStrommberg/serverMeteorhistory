335a7c3619db92292af26de239415b3085e3756e
Moritz <moe-stoll@web.de>
2017-12-07 18:35:07 +0100

    läuft erstmal

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-  res.send('HEY!')
+  res.send('HEY! Du hast den Server von Simon, Juli und Moritz angefragt! \n toll gemacht. Da bin ich nun und kann nichts für dich tun -.-')
 })
 app.listen(3000, () => console.log('Server running on 3000'))
