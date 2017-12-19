09b63a37f19af79b891255e24cc03c701f7562b8
Cantinaband <s.ruisinger@googlemail.com>
2017-12-19 15:22:05 +0100

    getContent
    add getContent query for mysql databse on aws

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const tasks = {
 
 function getContent(lat, lon) {
     let query =
-        "SELECT * FROM Meteor m LEFT JOIN events e on (m.lat = e.eventLat and m.lon = e.eventLon) left join Country c on (m.lat=c.countryLat and m.lon=c.countryLon) where m.lat between (" +
+        "SELECT * FROM Meteor m LEFT JOIN Event e on (m.lat = e.eventLat and m.lon = e.eventLon) left join Country c on (m.lat=c.countryLat and m.lon=c.countryLon) where m.lat between (" +
         (lat - radius) +
         ") and (" +
         (lat + radius) +
@@ -57,7 +57,6 @@ app.get("/", (request, response) => {
                 } else {
                     console.log("Hallo Meteoriten");
                     response.send(JSON.stringify(rows));
-
                 }
             }
         );
