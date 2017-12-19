c17c252ea35f998e6b54cd7b98a416277c063b83
Moritz <moe-stoll@web.de>
2017-12-19 16:13:19 +0100

    as

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //http: //ec2-18-195-17-121.eu-central-1.compute.amazonaws.com:8080/?task=createEvent&eventLat=.1002&eventLon=37.15&eventYear=01/01/1994 12:00:00 AM&eventCountryName=Heidepark Soltau&eventDescription=Test&eventLike=0&eventDislike=0&eventNickName=Moritz
 // http://ec2-18-195-17-121.eu-central-1.compute.amazonaws.com:8080/?task=createEvent&eventLat=-1.002&eventLon=37.15&eventYear=01/01/1994 12:00:00 20AM&eventCountryName=Heidepark 20Soltau&eventDescription=Test&eventLike=0&eventDislike=0&eventNickName=Moritz
 
-http://ec2-18-195-17-121.eu-central-1.compute.amazonaws.com:8080/?task=createEvent&eventLat=-1.002&eventLon=37.15&eventYear=2017&eventCountryName=HeideparkSoltau&eventDescription=Test&eventLike=0&eventDislike=0&eventNickName=Moritz
+//http://ec2-18-195-17-121.eu-central-1.compute.amazonaws.com:8080/?task=createEvent&eventLat=-1.002&eventLon=37.15&eventYear=2017&eventCountryName=HeideparkSoltau&eventDescription=Test&eventLike=0&eventDislike=0&eventNickName=Moritz
 const http = require("http");
 const mysql = require("mysql");
 const express = require("express");
@@ -73,7 +73,7 @@ function createEvent(
         "," +
         eventNickName +
         ")";
-        console.log(query);
+    console.log(query);
 
     return query;
 }
