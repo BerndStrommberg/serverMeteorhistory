4663e09e759c344187d8932b27d409005d85e645
Moritz <moe-stoll@web.de>
2017-12-19 16:01:41 +0100

    asa

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //http://ec2-18-195-17-121.eu-central-1.compute.amazonaws.com:3000/?task=getContent&lat=-1.002&lon=37.150
-//http: //ec2-18-195-17-121.eu-central-1.compute.amazonaws.com:8080/?task=createEvent&eventLat=.1002&eventLon=37.15&eventYear=01/01/1994 12:00:00 AM&eventCountryName=Heidepark Soltau&eventDescription=Test&eventLike=0,eventDisLike=0,eventNickName=Moritz
+//http: //ec2-18-195-17-121.eu-central-1.compute.amazonaws.com:8080/?task=createEvent&eventLat=.1002&eventLon=37.15&eventYear=01/01/1994 12:00:00 AM&eventCountryName=Heidepark Soltau&eventDescription=Test&eventLike=0&eventDislike=0&eventNickName=Moritz
+// http://ec2-18-195-17-121.eu-central-1.compute.amazonaws.com:8080/?task=createEvent&eventLat=-1.002&eventLon=37.15&eventYear=01/01/1994 12:00:00 20AM&eventCountryName=Heidepark 20Soltau&eventDescription=Test&eventLike=0&eventDislike=0&eventNickName=Moritz
 const http = require("http");
 const mysql = require("mysql");
 const express = require("express");
@@ -20,14 +21,14 @@ const tasks = {
     getContent: {
         name: "getContent"
     },
-    createEvent : {
+    createEvent: {
         name: "createEvent"
     }
 };
 
 function getContent(lat, lon) {
     let query =
-        "SELECT * FROM Meteor m LEFT JOIN Event e on (m.lat = e.eventLat and m.lon = e.eventLon and m.year = e.eventYear) left join Country c on (m.lat=c.countryLat and m.lon=c.countryLon) where m.lat between (" +
+        "SELECT * FROM Meteor m LEFT JOIN Event e on (m.lat = e.eventLat and m.lon = e.eventLon) left join Country c on (m.lat=c.countryLat and m.lon=c.countryLon) where m.lat between (" +
         (lat - radius) +
         ") and (" +
         (lat + radius) +
@@ -40,28 +41,36 @@ function getContent(lat, lon) {
     return query;
 }
 
-
-function createEvent(eventLat, eventLon, eventYear, eventCountryName, eventDescription, eventLike, eventDisLike, eventNickName) {
+function createEvent(
+    eventLat,
+    eventLon,
+    eventYear,
+    eventCountryName,
+    eventDescription,
+    eventLike,
+    eventDislike,
+    eventNickName
+) {
     let query =
-    "INSERT INTO Event (eventLat, eventLon, eventYear, eventCountryName, eventDescription, eventLike, eventDisLike, eventNickName) " +
-    "VALUES " +
-    "(" +
-    eventLat +
-    "," +
-    eventLon +
-    "," +
-    eventYear +
-    "," +
-    eventCountryName +
-    "," +
-    eventDescription +
-    "," +
-    eventLike +
-    "," +
-    eventDisLike +
-    "," +
-    eventNickName +
-    ")";
+        "INSERT INTO Event (eventLat, eventLon, eventYear, eventCountryName, eventDescription, eventLike, eventDislike, eventNickName) " +
+        "VALUES " +
+        "(" +
+        eventLat +
+        "," +
+        eventLon +
+        "," +
+        eventYear +
+        "," +
+        eventCountryName +
+        "," +
+        eventDescription +
+        "," +
+        eventLike +
+        "," +
+        eventDislike +
+        "," +
+        eventNickName +
+        ")";
 
     return query;
 }
@@ -92,8 +101,18 @@ app.get("/", (request, response) => {
         );
     } else if (query.task === tasks.createEvent.name) {
         connection.query(
-            createEvent(parseFloat(query.eventLat), parseFloat(query.eventLon), query.eventYear, query.eventCountryName, query.eventDescription, parseInt(query.eventLike), parseInt(query.eventDisLike), query.eventNickName), (err, rows, fields) => {
-                if(err) {
+            createEvent(
+                parseFloat(query.eventLat),
+                parseFloat(query.eventLon),
+                query.eventYear,
+                query.eventCountryName,
+                query.eventDescription,
+                parseInt(query.eventLike),
+                parseInt(query.eventDislike),
+                query.eventNickName
+            ),
+            (err, rows, fields) => {
+                if (err) {
                     console.log("Error: ", err);
                 } else {
                     response.send(query.eventNickName + ", Is deiner!");
